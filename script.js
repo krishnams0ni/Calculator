d327e388566af8c860c76960ce222a0a380fdff9
Krishnam Soni <krishnamsoni07@gmail.com>
2023-05-03 18:46:12 +0530

    Basic changes, trying to work out logic

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
 let numOne, numTwo, operator;
 
 function calculate(toCalc) {
-    toCalc = toCalc.split;
-    console.log(toCalc);
+    let prevNum,
+        num = 0;
+    let lastOperatorPosition = 0;
+    for (let i = 0; i <= toCalc.length; i++) {
+        if (
+            toCalc[i] === "-" ||
+            toCalc[i] === "+" ||
+            toCalc[i] === "*" ||
+            toCalc[i] === "/" ||
+            toCalc[i] === undefined
+        ) {
+            prevNum = toParse(lastOperatorPosition, i);
+            calc(parseInt(prevNum), parseInt(num), toCalc[lastOperatorPosition]);
+            num = prevNum;
+            lastOperatorPosition = i;
+        }
+    }
+
+    function toParse(start, end) {
+        let n = "";
+        for (let j = start; j < end; j++) {
+            n += toCalc[j];
+        }
+        return parseInt(n);
+    }
+
+    function calc(prevNum, num, operator) {
+        let n;
+        switch (operator) {
+            case "+":
+                n = prevNum + num;
+                break;
+            case "-":
+                n = prevNum - num;
+                break;
+            case "*":
+                n = prevNum * num;
+                break;
+            case "/":
+                n = prevNum / num;
+                break;
+        }
+        const displayUpdate = document.querySelector("#displayOutput");
+        displayUpdate.textContent = n;
+    }
 }
 
 const displayValue = document.querySelector(".calculate");
